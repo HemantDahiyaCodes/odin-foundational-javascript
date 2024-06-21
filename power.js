abf41e851eb2fc14a0c43d473859214cb49b30a1
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-06-21 19:38:54 +0530

    power.js

---
+++ power.js
@@ -0,0 +1,8 @@
+function power(num1, num2) {
+    return num1 ** num2;
+  }
+
+  let num_1 = +prompt("Enter the value of first number:");
+  let num_2 = +prompt("Enter the valueo f second number");
+
+  console.log(power(num_1, num_2));
